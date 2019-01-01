57f65f6acafbdf48cd30437b693e00cda4737af7
Sewef <unconfigured@null.spigotmc.org>
2019-01-01 17:45:29 +0100

    Unused import

--- src/main/java/com/sewef/woodstock/init/WoodStockBlocks.java
+++ src/main/java/com/sewef/woodstock/init/WoodStockBlocks.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
