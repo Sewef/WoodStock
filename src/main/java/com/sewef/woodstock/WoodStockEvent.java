6e62fa74d751c8137a4869eb98c5bd0039d8a7ed
Sewef <elagroum@gmail.com>
2019-05-09 10:27:32 +0200

    Better compat for modded axes

--- src/main/java/com/sewef/woodstock/WoodStockEvent.java
+++ src/main/java/com/sewef/woodstock/WoodStockEvent.java
@@ -22,41 +22,38 @@ public class WoodStockEvent {
     public void onRightClick(PlayerInteractEvent.RightClickBlock event) {
         World world = event.getWorld();
         BlockPos pos = event.getPos();
+        IBlockState state = world.getBlockState(pos);
         EntityPlayer player = event.getEntityPlayer();
         ItemStack stack = event.getItemStack();
         
-        if (stack.getItem() instanceof ItemTool) {
-            ItemTool tool = (ItemTool)stack.getItem();
-            if (tool.getToolClasses(stack).contains("axe")) {
-                IBlockState state = world.getBlockState(pos);
-                Block block = state.getBlock();
+        if (getHarvestLevel(stack, "axe", player, state) >= 0) {
+            Block block = state.getBlock();
+            
+            if (block == Blocks.LOG || block == Blocks.LOG2) {
+                IProperty axis = null, variant = null;
                 
-                if (block == Blocks.LOG || block == Blocks.LOG2) {
-                    IProperty axis = null, variant = null;
-                    
-                    for (IProperty<?> prop : state.getProperties().keySet()) {
-                        if (prop.getName().equals("axis")) {
-                            axis = prop;
-                        }
-                        
-                        if (prop.getName().equals("variant")) {
-                            variant = prop;
-                        }
+                for (IProperty<?> prop : state.getProperties().keySet()) {
+                    if (prop.getName().equals("axis")) {
+                        axis = prop;
                     }
                     
-                    if (axis != null && variant != null) {
-                        if (WoodStockBlocks.variants.contains(state.getValue(variant).toString())) {
-                            int i = WoodStockBlocks.variants.indexOf(state.getValue(variant).toString());
-                            
-                            if (world.isRemote) {
-                                player.swingArm(event.getHand());
-                                world.playSound(player, pos, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                                Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects(pos, state);
-                            }
-                            
-                            world.setBlockState(pos, WoodStockBlocks.strippedLogs.get(i).getDefaultState().withProperty(axis, state.getValue(axis)), 0b1011);
-                            stack.damageItem(1, player);
+                    if (prop.getName().equals("variant")) {
+                        variant = prop;
+                    }
+                }
+                
+                if (axis != null && variant != null) {
+                    if (WoodStockBlocks.variants.contains(state.getValue(variant).toString())) {
+                        int i = WoodStockBlocks.variants.indexOf(state.getValue(variant).toString());
+                        
+                        if (world.isRemote) {
+                            player.swingArm(event.getHand());
+                            world.playSound(player, pos, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                            Minecraft.getMinecraft().effectRenderer.addBlockDestroyEffects(pos, state);
                         }
+                        
+                        world.setBlockState(pos, WoodStockBlocks.strippedLogs.get(i).getDefaultState().withProperty(axis, state.getValue(axis)), 0b1011);
+                        stack.damageItem(1, player);
                     }
                 }
             }
